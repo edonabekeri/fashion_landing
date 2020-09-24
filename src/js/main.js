aed9f5d6fe7516202c7f5411be3e469ce5858557
edonabekeri <edonabekerikadiri@gmail.com>
2020-09-25 00:04:06 +0200

    second commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,5 @@
 $(document).ready(function() {
-  $('.banner_slider').slick({
-    infinite: false,
-    // slidesToShow: 2,
-    dots: true,
-    responsive: [
-  {
-    breakpoint: 1024,
-    settings: {
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      infinite: true,
-      dots: true
-    }
-  },
-  {
-    breakpoint: 600,
-    settings: {
-      slidesToShow: 2,
-      slidesToScroll: 2
-    }
-  },
-  {
-    breakpoint: 480,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-    }
-   }
-  })
+  $('.single-icon').click(function() {
+    $(this).toggleClass('animate');
+  });
 });
-
